872cb3b09a9aa03ded8a6757a4b3139209806823
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-01-26 14:24:06 +0900

    Feat: Express 테스트 앱 생성

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3005;
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${port}`);
+});
