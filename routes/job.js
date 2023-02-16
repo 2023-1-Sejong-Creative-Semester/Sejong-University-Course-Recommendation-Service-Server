078ce8e546db19900020278cac20831f0279b597
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-02-17 00:12:52 +0900

    Feat: 직업소개, 과목소개, 기업 트렌드 소개 기능 구현
    직업소개, 과목소개, 기업 트렌드 소개 기능 구현

--- routes/job.js
+++ routes/job.js
@@ -1,3 +1,49 @@
 const router = require('express').Router();
 
+//직업 소개
+router.get('/introduce',async(req,res)=>{
+
+    const job = req.query.job;
+
+    let intro, roadmap, language = [];
+
+    switch (job){
+        case "Front":
+            //나중에 DB 값 불러오기
+            intro = "프론트엔드 개발자는 ~~";
+            language = ["javascript"];
+            roadmap = "https://roadmap.sh/" + job;
+            break;
+            
+        case "Backend":
+            //나중에 DB 값 불러오기
+            intro = "백엔드 개발자는 ~~";
+            language = ["javascript", "JAVA", "Python"];
+            roadmap = "https://roadmap.sh/" + job;
+            break;
+            
+        case "App":
+            //나중에 DB 값 불러오기
+            intro = "어플리케이션 개발자는 ~~";
+            language = ["Swift", "JAVA", "Kotiln"];
+            roadmap = "https://roadmap.sh/" + job;
+            break;
+
+        case "AI":
+            //나중에 DB 값 불러오기
+            intro = "인공지능 개발자는 ~~";
+            language = ["Python", "JAVA", "javascript", "R", "Scala"];
+            roadmap = "https://roadmap.sh/" + job;
+            break;
+    }
+
+    res.json({
+        intro: intro,
+        language: language,
+        roadmap: roadmap
+    })
+
+});
+
+
 module.exports = router;
--- routes/trend.js
+++ routes/trend.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 
+//개발 언어 트렌드
+router.get('/trend',async(req,res)=>{
+
+    language = ["Java", "javascript", "aws"];
+    tech = ["Backend", "Front", "SW/Solution"];
+    
+    res.json({
+        language: language,
+        tech: tech
+    })
+
+});
+
 module.exports = router;
