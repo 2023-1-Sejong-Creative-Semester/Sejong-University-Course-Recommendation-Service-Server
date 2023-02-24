d04d78db466cd07c1b9a246202af846ffca2a715
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-02-24 16:10:03 +0900

    Modify: 채용 및 검색 트렌드 기능 수정

--- routes/trend.js
+++ routes/trend.js
@@ -3,12 +3,21 @@ const router = require('express').Router();
 //개발 언어 트렌드
 router.get('/trend',async(req,res)=>{
 
-    language = ["Java", "javascript", "aws"];
-    tech = ["Backend", "Front", "SW/Solution"];
+    recruit_language = ["Java", "javascript", "aws"];
+    recruit_tech = ["서버/백엔드", "프론트엔드", "SW/솔루션"];
+
+    search_language = ["Java", "React", "spring boot"];
+    search_tech = ["Flutter", "java", "프론트엔드"];
     
     res.json({
-        language: language,
-        tech: tech
+        recruit:{
+            language: recruit_language,
+            tech: recruit_tech
+        },
+        search: {
+            language: search_language,
+            tech: search_tech
+        }
     })
 
 });
