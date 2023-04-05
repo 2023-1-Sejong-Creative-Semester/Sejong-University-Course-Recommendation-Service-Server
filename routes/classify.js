40a7f7c91fcd2aede74be061b62dbd9f51ef4559
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-04-06 00:41:33 +0900

    Feat: classify/subject API 기초 작성

--- routes/classify.js
+++ routes/classify.js
@@ -6,32 +6,17 @@ const { param, route } = require('./introduce');
 router.post('/job',async(req,res)=>{
     try{
         const colleage = req.body.colleage;
-        const track = req.body.track;
-        const language = req.body.language;
+        const stack = req.body.stack;
         const category = req.body.category;
 
-        let param2;
         let SQL = "Select * from job_list where";
-    
-        //param 어캐줄 지 생각
-        switch(colleage){
-            case "전자정보통신공학대학":
-                SQL += "joblist.job = (select distinct(job_tag.job) from job_tag where stack LIKE ? and job_list.job = job_tag.job)";
-                SQL += " and cotegory = ?;";
-                param2 = track + '\r';
-                break;
-            case "소프트웨어융합대학":
-                SQL += "joblist.job = (select distinct(job_tag.job) from job_tag where stack LIKE ? and job_list.job = job_tag.job)";
-                param2 = language + '\r';
-                SQL += " and cotegory = ?;";
-            default:
-                break;
-        }
+        SQL += "joblist.job = ";
+        SQL += "( select distinct(job_tag.job) from job_tag where stack LIKE ? and job_list.job = job_tag.job )";
+        SQL += " and cotegory = ?;";
     
         const connection = db.return_connection();
-    
         
-        connection.query(SQL,[param2, category],function(err,results,field){
+        connection.query(SQL,[stack, category],function(err,results,field){
             console.log(results);
             return res.status(200).json({
                 results: results
@@ -48,14 +33,31 @@ router.post('/job',async(req,res)=>{
 })
 
 router.get('/subject',async(req,res)=>{
-    const department = (req.body.department !== undefined?req.body.department:'*');
-    const track = (req.body.track !== undefined?req.body.track:'*');
-    const semester = (req.body.semester !== undefined?req.body.semester:'*');
+    try{
+        const colleage = req.body.colleage;
+        const stack = req.body.stack;
+        const category = req.body.category;
 
-    let SQL = "Select * from re_main where collage = ? and department = ? and semester = ?and json_contains('track',?,'$.stack')";
-    connection.query(SQL,colleage, department, semester, track,function(err,results,field){
-        console.log(results);
-    })
+        let SQL = "Select * from job_list where";
+        SQL += "joblist.job = ";
+        SQL += "( select distinct(job_tag.job) from job_tag where stack LIKE ? and job_list.job = job_tag.job )";
+        SQL += " and cotegory = ?;";
+    
+        const connection = db.return_connection();
+        
+        connection.query(SQL,[stack, category],function(err,results,field){
+            console.log(results);
+            return res.status(200).json({
+                results: results
+            })
+        })
+    }
+    catch(err){
+        console.error(err);
+        return res.status(400).json({
+            error: err.toString()
+        })
+    }
 })
 
 module.exports = router;
